8f4852548babfc76c5fe77f9c684d972f950f8cc
Renato Mignogna <renmignogna@gmail.com>
2022-06-04 21:48:31 +0100

    initial boilerplate and store

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { Provider } from "react-redux"
+import Homescreen from "./screens/Homescreen"
+import { store } from "./store"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+        <Homescreen />
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ screens/components/NavOptions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  )
+}
+
+export default NavOptions
